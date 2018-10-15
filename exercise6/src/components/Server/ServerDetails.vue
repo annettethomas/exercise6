31d47cca7f6ed7e4673ad54ecf38b5b45dcf3945
annettethomas <42778997+annettethomas@users.noreply.github.com>
2018-10-14 23:14:42 -0600

    exercise6

---
+++ exercise6/src/components/Server/ServerDetails.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+
+        <p v-if="!server">Server Details are currently not updated</p>
+        <p v-else>Server #{{ server.id }} selected, Status: {{ server.status }}</p>
+        <hr>
+        <button @click="resetStatus">Change to normal</button>
+    </div>
+
+</template>
+
+<script>
+
+    import { serverBus } from "../../main.js";
+
+    export default {
+        data () {
+            return {
+                server: null
+            }
+        },
+
+        methods: {
+            resetStatus() {
+                this.server.status = "normal"
+            }
+        },
+        created (){
+            serverBus.$on('serverSelected', (server) => {
+                this.server = server;
+            })
+        }
+    }
+</script>
